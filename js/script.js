dac3c36ab92abf20be11ea92253730236c14fe25
MIGUEL GIL <miguel.gustavo.gil@gmail.com>
2024-01-25 15:01:14 -0300

    Fist Commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let N = 30
+
+function isPrime (num) {
+    if ( num <= 1 ){
+        return false ;     
+    }
+
+    for (let i = 2; i * i <= num ; i ++){
+        console.log('ix:',i);
+        if ( num % i === 0){
+        return false;
+        }
+    }
+
+    return true
+
+} 
+
+for (let i = 2; i <=N; i++){
+    if(isPrime(i)){
+        console.log(i)
+    }
+}
+
